86749a41aa5b9aa9e9813c6a15d882345f10f657
baijiayi <baijiayi@sinosoft.com.cn>
2020-07-18 21:29:03 +0800

    React 练习

---
+++ app/components/react/componentDemo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/**
+ * 第一种创建组件的形式
+ */
+function Hello(){
+    return <div> Hello Component</div>;
+}
+ReactDOM.render(
+    hi,
+    <Hello>
+    </Hello>,document.getElementById("apppp")
+);
